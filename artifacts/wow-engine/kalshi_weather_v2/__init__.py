a10907af6a1af6caf1389e6afcf280c2177d7ddc
gregoryharper84-ship-it <gregory.harper84@gmail.com>
2026-09-02 22:59:27 -0500

    Add Kalshi Weather V2 agent package

---
+++ artifacts/wow-engine/kalshi_weather_v2/__init__.py
@@ -0,0 +1,25 @@
+"""Governed Kalshi Weather V2 specialist agents and terminal reducer."""
+
+from .agents import ContractSettlementAgent, MarketCalibrationAuditor, WeatherProbabilityAgent
+from .models import (
+    AgentResult,
+    ContractSnapshot,
+    MarketSnapshot,
+    ProbabilityPackage,
+    TerminalDecision,
+    WeatherEvidenceSnapshot,
+)
+from .terminal_governor import KalshiWeatherTerminalGovernor
+
+__all__ = [
+    "AgentResult",
+    "ContractSnapshot",
+    "MarketSnapshot",
+    "ProbabilityPackage",
+    "TerminalDecision",
+    "WeatherEvidenceSnapshot",
+    "ContractSettlementAgent",
+    "WeatherProbabilityAgent",
+    "MarketCalibrationAuditor",
+    "KalshiWeatherTerminalGovernor",
+]
