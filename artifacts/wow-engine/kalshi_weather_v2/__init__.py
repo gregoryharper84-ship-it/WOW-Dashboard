ea9f43870ce7dad5f1da58a52f00cb96fbd2e6f0
gregoryharper84-ship-it <gregory.harper84@gmail.com>
2026-09-06 18:32:45 -0500

    Export weather observation and calibration research utilities

--- artifacts/wow-engine/kalshi_weather_v2/__init__.py
+++ artifacts/wow-engine/kalshi_weather_v2/__init__.py
@@ -1,6 +1,7 @@
 """Governed Kalshi Weather V2 specialist agents and terminal reducer."""
 
 from .agents import ContractSettlementAgent, MarketCalibrationAuditor, WeatherProbabilityAgent
+from .calibration_fit import CalibrationFitResult, ForecastResidual, fit_candidate_calibration_profile
 from .contract_resolver import ContractResolutionError, resolve_weather_contract
 from .models import (
     AgentResult,
@@ -10,6 +11,7 @@ from .models import (
     TerminalDecision,
     WeatherEvidenceSnapshot,
 )
+from .observation_reconstruction import ObservationPoint, ReconstructedExtreme, reconstruct_extreme, reconstruct_temperature_series
 from .orchestrator import evaluate_weather_contract
 from .probability_core import CalibrationProfile, WeatherProbabilityCore
 from .source_adapters import NoaaNceiAdapter, NwsAdapter, OpenMeteoAdapter, ProviderSnapshot, XweatherAdapter
@@ -30,6 +32,13 @@ __all__ = [
     "resolve_weather_contract",
     "CalibrationProfile",
     "WeatherProbabilityCore",
+    "ForecastResidual",
+    "CalibrationFitResult",
+    "fit_candidate_calibration_profile",
+    "ObservationPoint",
+    "ReconstructedExtreme",
+    "reconstruct_temperature_series",
+    "reconstruct_extreme",
     "ProviderSnapshot",
     "NwsAdapter",
     "OpenMeteoAdapter",
