e0004ee2a846e7513662affce31401909505fa14
gregoryharper84-ship-it <gregory.harper84@gmail.com>
2026-09-26 10:35:02 -0500

    V17: attach continuous auditor to worker lifetime

--- artifacts/wow-engine/agent_runtime/celery_app.py
+++ artifacts/wow-engine/agent_runtime/celery_app.py
@@ -5,5 +5,8 @@ pass. Run as: celery -A agent_runtime.celery_app:celery_app worker.
 from agent_runtime.queue import celery_app
 from agent_runtime import runner as _runner  # noqa: F401
 from agent_runtime import durable_runner as _durable_runner  # noqa: F401
+from v17.engineering_auditor_worker import install_celery_worker_hooks
+
+install_celery_worker_hooks()
 
 __all__ = ["celery_app"]
